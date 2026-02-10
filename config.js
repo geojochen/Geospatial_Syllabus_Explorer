dd3dd06f1f95c6f8e0c86e626290206e176e0e00
geojochen <jochen.albrecht@gmail.com>
2026-02-10 14:35:15 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// LIVE PRODUCTION KEYS
+const SUPABASE_CONFIG = {
+    url: "https://ygzxhhmckfyeywdfrwcp.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnenhoaG1ja2Z5ZXl3ZGZyd2NwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODQ3NzYxMCwiZXhwIjoyMDg0MDUzNjEwfQ.6OftFpbjyOJJNKL3q-KkcEBXH-Dvr0PJfh3LNVsdv4g"
+};
+Create config.js in the BETA folder:
+JavaScript
+// SANDBOX DEV KEYS
+const SUPABASE_CONFIG = {
+    url: "https://supabase.com/dashboard/project/yiplrmnbjridkydnvkve",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpcGxybW5ianJpZGt5ZG52a3ZlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDc0NjI2NywiZXhwIjoyMDg2MzIyMjY3fQ.g1P73py-rhytqhattVKYkam7WhisztRdCoAShCaONAU"
+};
