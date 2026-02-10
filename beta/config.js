f889ff6fbaeea24657f5f6929ca42f0f42e597d7
geojochen <jochen.albrecht@gmail.com>
2026-02-10 15:29:55 -0500

    Updating superbase config

--- beta/config.js
+++ beta/config.js
@@ -1,5 +1,5 @@
 // SANDBOX DEV KEYS
-const SUPABASE_CONFIG = {
+window.SUPABASE_CONFIG = {
     url: "https://supabase.com/dashboard/project/yiplrmnbjridkydnvkve",
     key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpcGxybW5ianJpZGt5ZG52a3ZlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDc0NjI2NywiZXhwIjoyMDg2MzIyMjY3fQ.g1P73py-rhytqhattVKYkam7WhisztRdCoAShCaONAU"
 };
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // LIVE PRODUCTION KEYS
-const SUPABASE_CONFIG = {
+window.SUPABASE_CONFIG = {
     url: "https://ygzxhhmckfyeywdfrwcp.supabase.co",
     key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnenhoaG1ja2Z5ZXl3ZGZyd2NwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODQ3NzYxMCwiZXhwIjoyMDg0MDUzNjEwfQ.6OftFpbjyOJJNKL3q-KkcEBXH-Dvr0PJfh3LNVsdv4g"
 };
