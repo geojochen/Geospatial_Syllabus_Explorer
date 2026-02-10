0cc264c550118d731b51d0a4f1a25c07afee5685
geojochen <jochen.albrecht@gmail.com>
2026-02-10 15:47:22 -0500

    Update config.js

--- beta/config.js
+++ beta/config.js
@@ -1,5 +1,5 @@
 // SANDBOX DEV KEYS
 window.SUPABASE_CONFIG = {
-    url: "https://yiplrmnbjridkydnvkve.supabase.co,
+    url: "https://yiplrmnbjridkydnvkve.supabase.co",
     key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnenhoaG1ja2Z5ZXl3ZGZyd2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0Nzc2MTAsImV4cCI6MjA4NDA1MzYxMH0.JrSW1-DyrduhK1_iAC-HgcwnxeDpzn98AVoriG0bTXw"
 };
